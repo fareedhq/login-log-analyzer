b5e30e8bec28948bbdb3c320aaf2b0e43cbd9c4b
fareedhq <fareedhq12@gmail.com>
2026-09-13 23:19:47 +0700

    counter unrecognized and check total

--- analyzer.py
+++ analyzer.py
@@ -9,10 +9,16 @@ login_status = [
     "success",
     "success",
     "failed",
+    "unknown",
+    "xss",
+    "unkown2",
+    "unknown3",
+    "unknown"
 ]
 
 success_count = 0
-failed_count = 0 
+failed_count = 0
+unrecognized_count = 0
 
 
 for status in login_status:
@@ -20,6 +26,12 @@ for status in login_status:
         success_count = success_count + 1
     elif status == "failed":
         failed_count = failed_count + 1
+    elif status != "success" and status != "failed":
+        unrecognized_count = unrecognized_count + 1
 
 print("Number of successful logins:", success_count)
 print("Number of failed logins:", failed_count)
+print("Number of unrecognized:", unrecognized_count)
+
+total_counted = success_count + failed_count + unrecognized_count
+print("total counted:", total_counted, "| total data:", len(login_status))
